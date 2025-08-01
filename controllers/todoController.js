acac5117daf5f7399c2ca4346b8784dd389176b7
Atilla1907 <atilla.gorgulu@chasacademy.se>
2025-08-01 15:57:20 +0200

    first commit

---
+++ controllers/todoController.js
@@ -0,0 +1,49 @@
+const Todo = require("../models/todoModel");
+
+exports.getAllTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.getTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).json({ error: "Not found" });
+    res.status(200).json(todo);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.createTodo = async (req, res) => {
+  try {
+    const newTodo = await Todo.create(req.body);
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ error: "Bad request" });
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const updated = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updated) return res.status(404).json({ error: "Not found" });
+    res.status(200).json(updated);
+  } catch (err) {
+    res.status(400).json({ error: "Bad request" });
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    const deleted = await Todo.findByIdAndDelete(req.params.id);
+    if (!deleted) return res.status(404).json({ error: "Not found" });
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
